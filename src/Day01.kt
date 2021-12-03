dd1f4bcc712a091d0747dc4e6c45234dc8f121c3
ochi <mune.ochi@gmail.com>
2021-12-03 13:21:07 +0900

    Day1 part1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,14 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
-    }
+        val size = input.size
 
-    fun part2(input: List<String>): Int {
-        return input.size
+        var count = 0
+        for (i in 0 until size-1) {
+            if (input[i+1].toInt() > input[i].toInt()) count += 1
+        }
+        return count
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
-    println(part2(input))
 }
