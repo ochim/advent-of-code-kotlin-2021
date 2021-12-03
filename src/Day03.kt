6d0d9c3993f49f6b7423a0743969b68baf6705d6
ochi <mune.ochi@gmail.com>
2021-12-03 19:34:26 +0900

    Day3 part1,part2

--- src/Day03.kt
+++ src/Day03.kt
@@ -13,7 +13,7 @@ fun main() {
         }
 
         array.forEach {
-            if (it > size/2) {
+            if (it.toFloat() > size.toFloat() / 2) {
                 gamma += "1"
                 epsilon += "0"
             } else {
@@ -25,7 +25,31 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val l = input[0].length
+        var temp = input.toMutableList()
+
+        for (i in 0 until l) {
+            var count = 0
+            temp.forEach{
+                if (it[i] == '1') count += 1
+            }
+            if (count.toFloat() >= temp.size.toFloat() / 2) temp = temp.filter { it[i] == '1' }.toMutableList() else temp = temp.filter { it[i] == '0' }.toMutableList()
+            if (temp.size == 1) break
+        }
+        val oxgen = temp.joinToString()
+
+        temp = input.toMutableList()
+        for (i in 0 until l) {
+            var count = 0
+            temp.forEach{
+                if (it[i] == '1') count += 1
+            }
+            if (count.toFloat() >= temp.size.toFloat() / 2) temp = temp.filter { it[i] == '0' }.toMutableList() else temp = temp.filter { it[i] == '1' }.toMutableList()
+            if (temp.size == 1) break
+        }
+        val co2 = temp.joinToString()
+
+        return oxgen.toInt(2) * co2.toInt(2)
     }
 
     val input = readInput("Day03")
