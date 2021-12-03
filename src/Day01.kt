dbe8b13d0ce84605b84e91a004cbf12dbe22197d
ochi <mune.ochi@gmail.com>
2021-12-03 13:45:18 +0900

    Day1 part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -9,6 +9,17 @@ fun main() {
         return count
     }
 
+    fun part2(input: List<String>): Int {
+        val size = input.size
+
+        var count = 0
+        for (i in 0 until size-3) {
+            if (input[i+3].toInt() > input[i].toInt()) count += 1
+        }
+        return count
+    }
+
     val input = readInput("Day01")
     println(part1(input))
+    println(part2(input))
 }
