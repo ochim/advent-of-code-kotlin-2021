6304064706313d605d0badc91b969e683f729c82
ochi <mune.ochi@gmail.com>
2021-12-06 02:40:43 +0900

    update Day1 code

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,25 +1,21 @@
+/* Day1
+ * https://blog.jetbrains.com/kotlin/2021/12/advent-of-code-2021-in-kotlin-day-1/
+ */
 fun main() {
-    fun part1(input: List<String>): Int {
-        val size = input.size
-
-        var count = 0
-        for (i in 0 until size-1) {
-            if (input[i+1].toInt() > input[i].toInt()) count += 1
-        }
-        return count
+    fun part1(input: List<Int>): Int {
+        return input.windowed(2).count { (a, b) -> a < b }
     }
 
-    fun part2(input: List<String>): Int {
-        val size = input.size
-
-        var count = 0
-        for (i in 0 until size-3) {
-            if (input[i+3].toInt() > input[i].toInt()) count += 1
-        }
-        return count
+    fun part2(input: List<Int>): Int {
+        return input.windowed(4).count { it[0] < it[3] }
     }
 
-    val input = readInput("Day01")
+    val testInput = readInputAsInts("Day01_test")
+    check(part1(testInput) == 7)
+
+    val input = readInputAsInts("Day01")
     println(part1(input))
+
+    check(part2(testInput) == 5)
     println(part2(input))
 }
