dd2b3068cbd5c37c486bc857b17474d54467d322
ochi <mune.ochi@gmail.com>
2021-12-16 22:16:39 +0900

    the day6 part1 and part2

--- src/Day06.kt
+++ src/Day06.kt
@@ -4,18 +4,35 @@ fun main() {
     fun part1(input: List<String>): Int {
         val fishTimers = input.first().split(",").map { it.toInt() }.toMutableList()
 
-        for (i in 1..80) {
+        repeat(80) {
             val zeros = fishTimers.count { it == 0 }
             for (j in fishTimers.indices) {
-                if (fishTimers[j] == 0) fishTimers[j] = 6 else fishTimers[j] -= 1
+                if (fishTimers[j] == 0) fishTimers[j] = 6 else fishTimers[j]--
             }
-            for (k in 0 until zeros) {
+            repeat(zeros) {
                 fishTimers.add(8)
             }
         }
         return fishTimers.size
     }
 
+    fun part2(input: List<String>): Long {
+        val fishTimers = input.first().split(",").map { it.toInt() }
+        val numByTimer = MutableList<Long>(9) { 0 }
+        fishTimers.forEach {
+            numByTimer[it]++
+        }
+
+        repeat(256) {
+            val zeros = numByTimer[0]
+            for (j in 0..7) {
+                numByTimer[j] = numByTimer[j + 1]
+            }
+            numByTimer[6] += zeros
+            numByTimer[8] = zeros
+        }
+        return numByTimer.sum()
+    }
 
     val testInput = readInput("Day06_test")
     val input = readInput("Day06")
@@ -23,4 +40,7 @@ fun main() {
     check(part1(testInput) == 5934)
     println("P1 Result: " + part1(input))
 
+    println("P2 Test: " + part2(testInput))
+    check(part2(testInput) == 26984457539)
+    println("P2 Result: " + part2(input))
 }
